3fa5ab07885e889cd5f120316f4a85f0e8ddab3c
owl0109 <kijimakenta19960109@gmail.com>
2020-12-25 14:35:15 +0900

    useEffectについて勉強

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React,{ useState } from 'react'
+import React,{ useEffect,useState } from 'react'
 
 const App = props =>{
 
   const [state, setState] = useState(props)
   const {name,price} = state
+
+  //レンダリング終了後、useEffectが走る
+  useEffect(() => {
+    console.log("UseEffect is invoked.")
+  })
+  
+  //空の配列を渡すと、初回のみ起動
+  useEffect(() => {
+    console.log("DidMount.")
+  },[])
+
+  //nameが変更された時に走る
+  useEffect(() => {
+    console.log("this call back is name onley.")
+  },[name])
 
   return (
     
