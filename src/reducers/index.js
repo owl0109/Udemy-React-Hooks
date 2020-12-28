bf2174cdeec58b75083776be0ce6d353ec6678ac
owl0109 <kijimakenta19960109@gmail.com>
2020-12-28 09:28:08 +0900

    クリエイトイベントを作成

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -43,7 +43,7 @@ const events =(state = [],action) => {
             const event = {title:action.title,body:action.body}
             const length = state.length
             const id = length === 0?  1 : state[length -1].id + 1
-            return [...state,{id: id,...event}]
+            return [...state,{id,...event}]
         case 'DELETE_EVENT':
             return state
         case 'DELETE_ALL_EVENT':
