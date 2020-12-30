11524d42028a55acc54ffa436934a154c077f37b
owl0109 <kijimakenta19960109@gmail.com>
2020-12-30 10:47:04 +0900

    ログ取得追加

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,11 @@
 import React,{ useState,useContext } from 'react'
-import {CREATE_EVENT,DELETE_ALL_EVENT} from '../actions'
+import {CREATE_EVENT,
+  DELETE_ALL_EVENT,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () =>{
   const {state,dispatch} = useContext(AppContext)
@@ -10,21 +15,33 @@ const EventForm = () =>{
 
   //e.prebentDefault()がかなり重要
   const addEvent = e => {
-  e.preventDefault()
-  //dispatch関数を呼ぶ
-  dispatch({
-      type: CREATE_EVENT,
-      title,
-      body 
-  })
-  setBody('')
-  setTitile('')
+    e.preventDefault()
+    //dispatch関数を呼ぶ
+    dispatch({
+        type: CREATE_EVENT,
+        title,
+        body 
+    })
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました',
+      operatedAt: timeCurrentIso8601()
+    })
+    
+    setBody('')
+    setTitile('')
   }
 
   const deleteAllEvents = e =>{
-  e.preventDefault()
-  const result = window.confirm('すべてのイベントを本当に削除してもよろしいですか？')
-  if(result) dispatch({type: DELETE_ALL_EVENT})
+    e.preventDefault()
+    const result = window.confirm('すべてのイベントを本当に削除してもよろしいですか？')
+    if(result) dispatch({type: DELETE_ALL_EVENT})
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'すべてのイベントを削除しました',
+      operatedAt: timeCurrentIso8601()
+    })
   }
 
   const unCreatable = title === '' || body === ''
