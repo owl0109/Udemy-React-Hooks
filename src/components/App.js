7da687b56ed13d2a7a90deda714fdc4d84006fb2
owl0109 <kijimakenta19960109@gmail.com>
2021-01-05 11:02:42 +0900

    ローカルストレージに保存済

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React , {useReducer} from 'react'
+import React , {useEffect,useReducer} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reducer from '../reducers'
 import EventForm from './EventForm'
@@ -6,16 +6,22 @@ import Events from './Events'
 import AppContext from '../contexts/AppContext'
 import OperationLogs from '../components/OperationLogs'
 
-console.log({AppContext})
+const APP_KEY = 'appWithRedux'
 
 const App = () =>{
+  const appState = localStorage.getItem(APP_KEY)
   //[配列(状態),関数(手段)]を渡す
-  const initialState = 
+  const initialState = appState ? JSON.parse(appState):
   {
     events:[],
     operationLogs:[]
   }
   const [state, dispatch] = useReducer(reducer,initialState)
+
+  useEffect(() => {
+    JSON.stringify(state)
+    localStorage.setItem(APP_KEY,JSON.stringify(state))
+  },[state])
 
   return (
     <AppContext.Provider value = {{ state,dispatch }}>
